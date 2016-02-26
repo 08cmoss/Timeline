3983631b8fcfa9dc6f391081d635e027107760be
Cameron Moss <08cmoss@gmail.com>
2016-02-25 17:39:27 -0700

    Part Two, up to View Hierarchy Details

---
+++ Timeline/ProfileHeaderCollectionReusableView.swift
@@ -0,0 +1,60 @@
+//
+//  ProfileHeaderCollectionReusableView.swift
+//  Timeline
+//
+//  Created by Cameron Moss on 2/25/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class ProfileHeaderCollectionReusableView: UICollectionReusableView {
+    
+    @IBOutlet weak var bioLabel: UILabel!
+    @IBOutlet weak var homepageButton: UIButton!
+    @IBOutlet weak var followButton: UIButton!
+    
+    var delegate: ProfileHeaderCollectionReusableViewDelegate?
+    
+    @IBAction func urlButtonTapped(sender: AnyObject) {
+        delegate?.userTappedURLButton()
+        
+    }
+    
+    
+    @IBAction func followUserButtonTapped(sender: AnyObject) {
+        delegate?.userTappedFollowActionButton()
+    }
+    
+    
+    
+    func updateWithUser(user: User) {
+        if user.bio != nil {
+            bioLabel.text = user.bio
+        } else {
+            bioLabel.hidden = true
+        }
+        if user.url != nil {
+            homepageButton.setTitle(user.url, forState: .Normal)
+        } else {
+            homepageButton.hidden = true
+        }
+        if user == UserController.sharedController.currentUser {
+            followButton.hidden = true
+        } else{
+            followButton.hidden = false
+            UserController.userFollowsUser(UserController.sharedController.currentUser, user2: user, completion: { (follows) -> Void in
+                if follows {
+                    self.followButton.setTitle("Unfollow", forState: .Normal)
+                } else {
+                    self.followButton.setTitle("Follow", forState: .Normal)
+                }
+            })
+        }
+    }
+}
+
+protocol ProfileHeaderCollectionReusableViewDelegate {
+    func userTappedFollowActionButton ()
+    func userTappedURLButton()
+}
--- Timeline/ProfileViewController.swift
+++ Timeline/ProfileViewController.swift
@@ -8,9 +8,12 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate{
     
+    
+    @IBOutlet weak var collectionView: UICollectionView!
     var user: User?
+    var userPosts: [Post] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +26,41 @@ class ProfileViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return userPosts.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let postCell = collectionView.dequeueReusableCellWithReuseIdentifier("collectionCell", forIndexPath: indexPath) as! ImageCollectionViewCell
+        let post = userPosts[indexPath.item]
+        postCell.updateWithImageIdentifier(post.identifier ?? "")
+        return postCell
+    }
+    
+    func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
+        let headerCell =  collectionView.dequeueReusableSupplementaryViewOfKind("ProfileHeaderCollectionReusableView", withReuseIdentifier: "headerCell", forIndexPath: indexPath) as! ProfileHeaderCollectionReusableView
+        if let user = user {
+        headerCell.updateWithUser(user)
+        }
+        return headerCell
+    }
+    
+    
+    
+    func updateBasedOnUser() {
+        self.title = user?.username
+        if let user = user {
+        PostController.fetchTimelineForUser(user) { (results) -> Void in
+            self.userPosts = results
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                self.collectionView.reloadData()
+            })
+            
+            }
+        }
     }
     
 
